7fcc0cf5ca702659e866c7864b191d2520885b3e
Trey Manuszak <manuszaktrey@gmail.com>
2023-04-26 14:31:31 -0700

    cleanup

--- tail.py
+++ tail.py
@@ -12,33 +12,26 @@ NUM_ROUNDS = 10  # number of rounds with randomized n in each trial
 MAX_SEQ_SIZE = 10 ** 8
 
 # simple slice
-def tail_v1(sequence, n):
+def tail_slicing(sequence, n):
 	return sequence[-n:]
 
 # append one by one
-def tail_v2(sequence, n):
+def tail_appending(sequence, n):
 	result = []
 	for i in range(-n, 0):
 		result.append(sequence[i])
 	return result
 
 # List comp
-def tail_v3(sequence, n):
+def tail_list_comp(sequence, n):
 	return [sequence[-i] for i in range(n, 0, -1)]
 
 # Deque for fun
-def tail_v4(sequence, n):
+def tail_deque(sequence, n):
 	return deque(sequence, maxlen=n)
 
-# Return a generator for sequence. ONLY USE IF SEQUENCE WONT CHANGE AFTER CALL!
-def tail_v5(sequence, n):
-	def generator():
-		for index in range(n, 0, -1):
-			yield sequence[-index]
-	return generator()
-
 # a space efficient generator for sequence. ONLY USE IF SEQUENCE WONT CHANGE AFTER CALL!
-def tail_v6(sequence, n):
+def tail_return_generator(sequence, n):
 	def generator():
 		index = n + 1
 		while index > 1:
@@ -46,13 +39,23 @@ def tail_v6(sequence, n):
 			yield sequence[-index]
 	return generator()
 
+# Similar to tail_v5, but returns a list rather than generator. Params are safe to change.
+def tail_generator_plus_list(sequence, n):
+	def generator():
+		index = n + 1
+		while index > 1:
+			index -= 1
+			yield sequence[-index]
+	return list(generator())
+
 # called if sequence is an np array.
 # Same implementation as v1. Just done this way due to plot implementation.
-def tail_numpy(sequence, n):
+def tail_numpy_slicing(sequence, n):
 	return sequence[-n:]
 
 # name of functions that are profiled
-functions = [tail_v1, tail_v2, tail_v3, tail_v4, tail_v5, tail_v6, tail_numpy]
+functions = [tail_slicing, tail_appending, tail_list_comp, tail_deque, 
+	tail_return_generator, tail_generator_plus_list, tail_numpy_slicing]
 
 # Returning trials-many running times of each function with sequence of 
 # total_numbers ints and a random (valid) n.
@@ -65,15 +68,20 @@ def profile(sequence_size, prof_data):
 		n = random.randint(0, sequence_size)
 		# profile each function
 		for function in functions:
-			filename = "{}.prof".format(function.__name__)
+			filename = f"{function.__name__}.prof"
 			# if sequence is numpy or not
-			if function.__name__ == "tail_numpy":
-				seq = sequence_np
+			pf = None
+			if function == functions[-1]:
+				pf = cProfile.runctx(
+					statement=f'{function.__name__}(sequence, n)', 
+					globals={f'{function.__name__}': function},
+					locals={'sequence': sequence_np, 'n': n},
+					filename=filename)
 			else:
-				seq = sequence
-			pf = cProfile.runctx(statement='{}(sequence, n)'.format(function.__name__), 
-					globals={'{}'.format(function.__name__): function},
-					locals={'sequence': seq, 'n': n},
+				pf = cProfile.runctx(
+					statement=f'{function.__name__}(sequence, n)', 
+					globals={f'{function.__name__}': function},
+					locals={'sequence': sequence, 'n': n},
 					filename=filename)
 			stats = pstats.Stats(filename)
 			prof_data[function.__name__].append(stats.total_tt)
@@ -100,14 +108,14 @@ def plot_prof_data(prof_data):
 
 	# plot metadata
 	ax.legend()
-	ax.set_title('Plot Title')
+	ax.set_title('Running Time Profiles of Tail Functions (Intel i7-7700K @ 4.2GHz)')
 	ax.set_xlabel('Sequence Length (log scale)')
 	ax.set_ylabel('Running Time (log scale)')
 	ax.set_xscale('log', base=10)
 	ax.set_yscale('log', base=10)
 
 	plt.savefig('/mnt/c/Users/Trey/Downloads/myplot.png')
-	plt.savefig('./myplot.png')
+	plt.savefig('./time-profile-plot.png')
 	return
 
 if __name__ == "__main__":
